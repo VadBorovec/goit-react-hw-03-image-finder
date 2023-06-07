70bf38678b410d6aec78e94f39c6046b69d1babd
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-06-07 16:11:12 +0300

    add UI files

---
+++ src/components/ui/IconButton/IconButton.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { IconBtn } from './IconButton.styled';
+
+export const IconButton = ({ children, onClick, ...allyProps }) => (
+  <IconBtn
+    type="button"
+    className="IconButton"
+    onClick={onClick}
+    {...allyProps}
+  >
+    {children}
+  </IconBtn>
+);
+
+IconButton.defaultProps = {
+  onClick: () => null,
+  children: null,
+};
+
+IconButton.propTypes = {
+  onClick: PropTypes.func,
+  children: PropTypes.node,
+  'aria-label': PropTypes.string.isRequired,
+};
